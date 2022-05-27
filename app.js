ffe1165c752de1a0d559744c603b282898fd53a0
Nabaraj <nabaraj.dhl77@gmail.com>
2022-05-27 16:26:39 +0545

    User input and Random No checking Completed

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+
+//gaming
+//Actual code
+//Generating Random Number
+
+document.querySelector('.check').addEventListener('click', function () {
+  let ranNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.guess-no-shower').textContent = ranNumber;
+  console.log(ranNumber);
+  const userNumber = Number(document.querySelector('.guess-no').value);
+  console.log(userNumber);
+  if (ranNumber === userNumber) {
+    document.querySelector('.guess-message').textContent = 'Correct Guess...';
+  }
+});
