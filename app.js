3f0cfd43e73398260df009625dd0d23abbf729ee
Nabaraj <nabaraj.dhl77@gmail.com>
2022-05-27 19:29:45 +0545

    Game completed

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
 
 //gaming
 //Actual code
 //Generating Random Number
 let score = 20;
+let highScore = 0;
+document.querySelector('.high-score').textContent = highScore;
 let ranNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.guess-no-shower').textContent = ranNumber;
 document.querySelector('.check').addEventListener('click', function () {
-  document.querySelector('.guess-no-shower').textContent = ranNumber;
-  console.log(ranNumber);
   const userNumber = Number(document.querySelector('.guess-no').value);
-  console.log(userNumber);
+  //console.log(userNumber);
   if (!userNumber) {
     document.querySelector('.guess-message').textContent = 'Enter a no.';
   } else if (ranNumber === userNumber) {
+    document.querySelector('.guess-no-shower').textContent = ranNumber;
     document.querySelector('.guess-message').textContent = 'Correct Answer...';
+    document.querySelector('.wrapper').style.backgroundColor = '#60b347';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.high-score').textContent = highScore;
+    }
   } else if (userNumber > ranNumber) {
     if (score > 1) {
       score--;
@@ -35,4 +41,14 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score-given').textContent = '0';
     }
   }
+});
+document.querySelector('.play-again').addEventListener('click', function () {
+  score = 20;
+  let ranNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.score-given').textContent = score;
+  document.querySelector('.guess-no-shower').textContent = '?';
+  document.querySelector('.guess-message').textContent = 'Start guessing...';
+  document.querySelector('.guess-no').textContent = '';
+  document.querySelector('.wrapper').style.backgroundColor = '#ff6347';
+  document.querySelector('.guess-no').value = '';
 });
